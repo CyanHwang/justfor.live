7d4c6a095840de599a217f0a3c09a0585b66fb9f
Cyan <541448803@qq.com>
2019-08-24 12:20:29 +0800

    修改布局

---
+++ web/precache-manifest.76bb5d446837d2c9f6d365a49a40e1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1899fb9e9d82a9314fabf6934110dda7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d9cb83feb1244ac962",
+    "url": "/static/css/2.ee4f8ef5.chunk.css"
+  },
+  {
+    "revision": "c724e504549ab33bbb8d",
+    "url": "/static/css/main.0f161524.chunk.css"
+  },
+  {
+    "revision": "e9d9cb83feb1244ac962",
+    "url": "/static/js/2.0589c8d7.chunk.js"
+  },
+  {
+    "revision": "c724e504549ab33bbb8d",
+    "url": "/static/js/main.98fa49a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
